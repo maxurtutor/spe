ad93860d74cb24f843cbbe77644f9a8bb1679dca
Maxim Yunusov <maximyunusov@gmail.com>
2014-10-05 23:25:28 +0700

    Remove Transaction Managment

--- spe-service/src/main/java/org/maxur/spe/service/SendMailService.java
+++ spe-service/src/main/java/org/maxur/spe/service/SendMailService.java
@@ -10,13 +10,8 @@ import org.maxur.spe.infrastructure.ConnectionFactoryJDBCImpl;
 import org.maxur.spe.infrastructure.MailIdServiceJDBCImpl;
 import org.maxur.spe.infrastructure.MailRepositoryJDBCImpl;
 import org.maxur.spe.infrastructure.MailServiceJavaxImpl;
-import org.slf4j.Logger;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-
-import static org.maxur.spe.service.ConnectionWrapper.wrap;
-import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * @author Maxim
@@ -24,8 +19,6 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @since <pre>05.10.2014</pre>
  */
 public class SendMailService {
-
-    private static Logger LOGGER = getLogger(SendMailService.class);
 
     public static final String FROM_ADDRESS = "sender@here.com";
 
@@ -35,60 +28,13 @@ public class SendMailService {
         factory = new ConnectionFactoryJDBCImpl();
     }
 
-    public String send(String message) {
-        Connection connection = startTransaction();
-        try {
-            MailService mailService = new MailServiceJavaxImpl(FROM_ADDRESS);
-            Repository<Mail> repository = new MailRepositoryJDBCImpl(() -> wrap(connection));
-            MailIdService idService = new MailIdServiceJDBCImpl(() -> wrap(connection));
-            Worker worker = new Worker(mailService, repository, idService);
-            String result = worker.run(message);
-            commit(connection);
-            return result;
-        } finally {
-            endTransaction(connection);
-        }
+    public synchronized String send(String message) {
+        MailService mailService = new MailServiceJavaxImpl(FROM_ADDRESS);
+        Repository<Mail> repository = new MailRepositoryJDBCImpl(factory);
+        MailIdService idService = new MailIdServiceJDBCImpl(factory);
+        Worker worker = new Worker(mailService, repository, idService);
+        return worker.run(message);
     }
 
-    private void endTransaction(Connection connection) {
-        try {
-            connection.setAutoCommit(true);
-            connection.close();
-        } catch (SQLException e) {
-            LOGGER.error("Connection is not closed", e);
-            throw new IllegalStateException("Connection is not closed", e);
-        }
-    }
-
-    private void commit(Connection connection) {
-        try {
-            connection.commit();
-        } catch (SQLException e) {
-            rollback(connection);
-            throw new IllegalStateException("Transaction is not committed", e);
-        }
-    }
-
-    private void rollback(Connection connection) {
-        try {
-            System.err.print("Transaction is being rolled back");
-            connection.rollback();
-        } catch(SQLException e) {
-            LOGGER.error("Transaction is not rolled back", e);
-            throw new IllegalStateException("Transaction is not rolled back", e);
-        }
-    }
-
-    private Connection startTransaction() {
-        try {
-            Connection connection = factory.get();
-            // connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE); Deadlock !
-            connection.setAutoCommit(false);
-            return connection;
-        } catch (SQLException e) {
-            LOGGER.error("Transaction is not started", e);
-            throw new IllegalStateException("Transaction is not started", e);
-        }
-    }
 
 }
