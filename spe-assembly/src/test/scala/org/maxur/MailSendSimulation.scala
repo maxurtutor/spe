518b1e7e214e8bed7be0e2f9aaf1fbcc19a40f11
myunusov <myunusov@luxoft.com>
2014-10-07 15:27:56 +0700

    Objectivity Add gatling

---
+++ spe-assembly/src/test/scala/org/maxur/MailSendSimulation.scala
@@ -0,0 +1,33 @@
+package org.maxur
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class MailSendSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://localhost:9090")
+		.inferHtmlResources()
+		.acceptHeader("""*/*""")
+		.acceptEncodingHeader("""gzip,deflate""")
+		.contentTypeHeader("""application/x-www-form-urlencoded; charset=windows-1251""")
+		.userAgentHeader("""Apache-HttpClient/4.3.2 (java 1.5)""")
+
+	val headers_0 = Map("""Cache-Control""" -> """no-cache""")
+
+    val uri1 = """localhost"""
+
+	val scn = scenario("RecordedSimulation")
+             .repeat(100) {
+		 exec(http("request_0")
+			.post("""/service/mail""")
+			.headers(headers_0)
+			.formParam("""message""", """Hello"""))
+			.pause(100 milliseconds)
+	      }
+
+	setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
+}
