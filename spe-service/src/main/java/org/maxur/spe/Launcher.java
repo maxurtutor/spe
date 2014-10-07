3a52e8976a4771fc8f06d3df93458ceb3ccc79af
myunusov <myunusov@luxoft.com>
2014-10-07 13:21:58 +0700

    draft

--- spe-service/src/main/java/org/maxur/spe/Launcher.java
+++ spe-service/src/main/java/org/maxur/spe/Launcher.java
@@ -1,12 +1,6 @@
 package org.maxur.spe;
 
-import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
-import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
-import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -14,9 +8,6 @@ import org.maxur.spe.service.SendMailService;
 import org.slf4j.Logger;
 
 import java.net.URI;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -57,27 +48,6 @@ public class Launcher {
     private void startWebServer() {
         LOGGER.info("Start Grizzly");
         httpServer = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, createApp());
-
-        httpServer.getServerConfiguration().addHttpHandler(
-                new HttpHandler() {
-                    public void service(Request request, Response response) throws Exception {
-                        final SimpleDateFormat format =
-                                new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
-                        final String date = format.format(new Date(System.currentTimeMillis()));
-                        response.setContentType("text/plain");
-                        response.setContentLength(date.length());
-                        response.getWriter().write(date);
-                    }
-                },
-                "/time");
-
-        final TCPNIOTransportBuilder builder = TCPNIOTransportBuilder.newInstance();
-        final ThreadPoolConfig config = builder.getWorkerThreadPoolConfig();
-        final ThreadPoolConfig threadPoolConfig = config.setCorePoolSize(5).setMaxPoolSize(5).setQueueLimit(-1);
-        final TCPNIOTransport transport = builder.build();
-
-
-
     }
 
     public ResourceConfig createApp() {
